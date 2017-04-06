06f535f314d587718b441a8a45046c301d701ae7
zhongying <diaozy@gmail.com>
2017-04-07 00:13:46 +0800

    up

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,6 @@
 //index.js
 //获取应用实例
+var px2rpx = 2, windowWidth=375
 var app = getApp()
 Page({
   data: {
@@ -7,30 +8,35 @@ Page({
     poster: '',
     name: '一课一练',
     author: 'Susan英语学堂',
-    src: 'http://susan-1253455298.cossh.myqcloud.com/SBS/SBS1%E7%BB%83%E4%B9%A0%E5%86%8C/A001Introduction%20.MP3'
-
+    src: 'http://susan-1253455298.cossh.myqcloud.com/SBS/SBS1%E7%BB%83%E4%B9%A0%E5%86%8C/A001Introduction%20.MP3',
+ pictures: [ 'http://susan-1253455298.cossh.myqcloud.com/images/niujin22.jpg',
+ 'http://susan-1253455298.cossh.myqcloud.com/images/sbs1.jpg','http://susan-1253455298.cossh.myqcloud.com/images/sbs1.jpg' ],
+  imageSize:{},
+  index:[1,2,3],
+  ppp:''
   },
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+
   onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+ wx.getSystemInfo({
+ success: function(res) {
+ windowWidth = res.windowWidth;
+ px2rpx = 750 / windowWidth;
+ }
+ })
   },
+
+bindtapgo:function()
+{
+index = e.currentTarget.dataset.index
+this.setdata({ppp:index})
+
+},
 
  onReady: function (e) {
     // 使用 wx.createAudioContext 获取 audio 上下文 context
     this.audioCtx = wx.createAudioContext('myAudio')
+
   },
 
   audioPlay: function () {
@@ -44,7 +50,43 @@ Page({
   },
   audioStart: function () {
     this.audioCtx.seek(0)
-  }
+  },
+
+ previewImage: function(e){
+ var that = this,
+ index = e.currentTarget.dataset.index,
+ pictures = this.data.pictures;
+ wx.previewImage({
+ current: pictures[index],
+ urls: pictures
+ })
+ },
+
+ imageLoad:function(e){
+ //单位rpx
+ var originWidth = e.detail.width*px2rpx, 
+ originHeight = e.detail.height*px2rpx,
+ ratio = originWidth/originHeight;
+ var viewWidth = 710,viewHeight//设定一个初始宽度
+ //当它的宽度大于初始宽度时，实际效果跟mode=widthFix一致
+ if(originWidth>= viewWidth){
+ //宽度等于viewWidth,只需要求出高度就能实现自适应
+ viewHeight = viewWidth/ratio;
+ }else{
+ //如果宽度小于初始值，这时就不要缩放了
+ viewWidth = originWidth;
+ viewHeight = originHeight;
+ }
+ var imageSize = this.data.imageSize;
+ imageSize[e.target.dataset.index] = {
+ width:viewWidth,
+ height:viewHeight
+ }
+ this.setData({
+ imageSize:imageSize
+ })
+ }
+
 })
 
 
