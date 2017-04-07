4902bdfd3595505f93fe4c1552d6b049c32d92b4
diaozy <diaozy@gmail.com>
2017-04-07 17:30:18 +0800

    up

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,5 @@
 //index.js
 //获取应用实例
-var px2rpx = 2, windowWidth=375
 var app = getApp()
 Page({
   data: {
@@ -9,28 +8,26 @@ Page({
     name: '一课一练',
     author: 'Susan英语学堂',
     src: 'http://susan-1253455298.cossh.myqcloud.com/SBS/SBS1%E7%BB%83%E4%B9%A0%E5%86%8C/A001Introduction%20.MP3',
- pictures: [ 'http://susan-1253455298.cossh.myqcloud.com/images/niujin22.jpg',
- 'http://susan-1253455298.cossh.myqcloud.com/images/sbs1.jpg','http://susan-1253455298.cossh.myqcloud.com/images/sbs1.jpg' ],
-  imageSize:{},
-  index:[1,2,3],
-  ppp:''
+ pictures: [ 
+  'http://susan-1253455298.cossh.myqcloud.com/images/sbs1.jpg', 
+  'http://susan-1253455298.cossh.myqcloud.com/images/niujin22.jpg'
+ ]
+
   },
   //事件处理函数
 
-  onLoad: function () {
- wx.getSystemInfo({
- success: function(res) {
- windowWidth = res.windowWidth;
- px2rpx = 750 / windowWidth;
- }
- })
+ onLoad: function () {
+
   },
 
-bindtapgo:function()
+bindtapgo0:function()
 {
-index = e.currentTarget.dataset.index
-this.setdata({ppp:index})
+wx.navigateTo({url: '../page0/page0'})
+},
 
+bindtapgo1:function()
+{
+wx.navigateTo({url: '../page1/page1'})
 },
 
  onReady: function (e) {
@@ -60,32 +57,8 @@ this.setdata({ppp:index})
  current: pictures[index],
  urls: pictures
  })
- },
+ }
 
- imageLoad:function(e){
- //单位rpx
- var originWidth = e.detail.width*px2rpx, 
- originHeight = e.detail.height*px2rpx,
- ratio = originWidth/originHeight;
- var viewWidth = 710,viewHeight//设定一个初始宽度
- //当它的宽度大于初始宽度时，实际效果跟mode=widthFix一致
- if(originWidth>= viewWidth){
- //宽度等于viewWidth,只需要求出高度就能实现自适应
- viewHeight = viewWidth/ratio;
- }else{
- //如果宽度小于初始值，这时就不要缩放了
- viewWidth = originWidth;
- viewHeight = originHeight;
- }
- var imageSize = this.data.imageSize;
- imageSize[e.target.dataset.index] = {
- width:viewWidth,
- height:viewHeight
- }
- this.setData({
- imageSize:imageSize
- })
- }
 
 })
 
---
+++ pages/page0/page0.js
@@ -0,0 +1,31 @@
+// pages/page1/page1.js
+Page({
+  data:{
+
+    name: '朗文国际英语',
+    author: 'Susan英语学堂',
+    src: 'http://susan-1253455298.cossh.myqcloud.com/SBS/SBS1%E7%BB%83%E4%B9%A0%E5%86%8C/A001Introduction%20.MP3',
+ pictures: [ 
+  'http://susan-1253455298.cossh.myqcloud.com/images/sbs1.jpg', 
+  'http://susan-1253455298.cossh.myqcloud.com/images/niujin22.jpg'
+ ]
+
+
+
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})
