b9fd04add7c6a411d582102f816167046f9720b9
zhongying <diaozy@gmail.com>
2017-04-09 23:37:01 +0800

    up

---
+++ utils/data.js
@@ -0,0 +1,29 @@
+
+const songs = {
+	
+"000001": {
+	"mp3Url":"http://susan-1253455298.cossh.myqcloud.com/SBS/SBS1%E8%AF%BE%E6%9C%AC/A001Introduction%20.MP3",	
+	album: {	
+		picUrl: "http://susan-1253455298.cossh.myqcloud.com/images/sbs1.jpg",
+		name: "国际朗文英语第1册",
+	},	
+	artists: "Susan英语学堂",	
+	name: "A001Introduction  ",	
+	zh: ""	
+},		
+"0000002": {
+	"mp3Url":"http://susan-1253455298.cossh.myqcloud.com/SBS/SBS1%E8%AF%BE%E6%9C%AC/P001_Chapter%2001_Vocabulary%20Preview.mp3",	
+	album: {	
+		picUrl: "http://susan-1253455298.cossh.myqcloud.com/images/sbs1.jpg",
+		name: "国际朗文英语第1册",
+	},	
+	artists: "Susan英语学堂",	
+	name: "P001_Chapter 01_Vocabulary Preview   ",	
+	zh: ""	
+},		
+
+}
+
+module.exports = {
+	songs: songs
+}
